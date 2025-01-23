59857083e2abf08d640f3c8d841a3af40eda737c
nischalgautambaylor <nischal_gautam1@baylor.edu>
2025-01-22 22:42:45 -0600

    changed avatar

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -37,7 +37,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid" alt="avatar2" />
             </Tilt>
           </Col>
         </Row>
