983d8fb8970fe174313c0536cbaca8f3ec2ff9c8
nischalgautambaylor <nischal_gautam1@baylor.edu>
2025-01-23 15:24:22 -0600

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,29 +8,31 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Nischal Binod Gautam </span>
-            from <span className="purple"> Waco, Texas,United States. </span>
+            Hi Everyone, I am <span className="purple">Nischal Gautam, </span>
+            a PhD student at <span className="purple"> Baylor University. </span>
             <br />
             <br />
-            I am an Electrical engineer with over 5 years of experience in low-voltage systems,
-            integrating technical knowledge with sales and project management abilities.
-            Driven to perform in tough tasks and make significant contributions to organisational success.
+            I hold a Bachelor's degree in Electrical Engineering from Nepal 
+            and am currently pursuing a PhD in Electrical and Computer Engineering at 
+            Baylor University. My academic journey has been driven by a 
+            deep passion for exploring the cutting-edge field of quantum computing. 
+            Through my research, I aim to contribute to advancements in this 
+            transformative technology and its potential applications. 
+            Beyond academics, I enjoy engaging in projects and discussions that 
+            push the boundaries of innovation and discovery.
             <br />
             <br />
-            I am recently studying at Baylor University.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+              Some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Sports
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
             <li className="about-activity">
-              <ImPointRight /> Charity work
+              <ImPointRight /> Reading Books
             </li>
           </ul>
         </blockquote>
